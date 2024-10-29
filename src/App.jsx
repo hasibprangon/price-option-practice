248a9f57f6195fe1f78f813f74742b8cd9344928
hasibprangon <hasibulhossainp09@gmail.com>
2024-10-29 20:01:22 +0600

    creating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import NavBar2 from './Components/NavBar2/NavBar2'
 
 function App() {
 
   return (
     <>
-      
+      {/* <Navbar></Navbar> */}
+      <NavBar2></NavBar2>
       <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
      
     </>
---
+++ src/Components/NavBar2/NavBar2.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from '../Link/Link';
+
+const NavBar2 = () => {
+    const routes = [
+        { id: 1, name: "Home", path: "/" },
+        { id: 2, name: "About", path: "/about" },
+        { id: 3, name: "Services", path: "/services" },
+        { id: 4, name: "Contact", path: "/contact" },
+        { id: 5, name: "Profile", path: "/profile" },
+    ];
+    return (
+        <div>
+            <ul className='md:flex'>
+                {
+                    routes.map(route => <Link
+                        key={route.id}
+                        route={route}
+                    ></Link>)
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default NavBar2;
